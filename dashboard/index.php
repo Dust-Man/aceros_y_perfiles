63bb945d444e4a26e7b801eeb9d5c8053170b533
Dust-Man <carlosgaelhp@gmail.com>
2024-11-27 07:28:03 -0600

    index del dashboard

---
+++ dashboard/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Panel de Administración CA</title>
+
+    <link rel="stylesheet" href="../css/dashboard.css">
+</head>
+<body>
+    <div class="dashboard">
+        <!-- Sidebar -->
+        <div class="sidebar">
+            <div class="logo">Panel de Administración CA</div>
+            <ul class="menu">
+                <li><a href="#">Dashboard</a></li>
+                <li><a href="#">Registrar producto</a></li>
+                <li><a href="#">Crear Post</a></li>
+                <li><a href="#">Generar Nota</a></li>
+                <li><a href="#">Generar Envío</a></li>
+                <li><a href="#">Productos por Enviar</a></li>
+                <li><a href="#">Registrar cliente</a></li>
+                <li><a href="#">Registrar empledo</a></li>
+                <li><a href="#">Registrar vehículo</a></li>
+            </ul>
+            <div class="rol">Rol: </div>
+        </div>
+        
+        <!-- Main Content -->
+        <div class="main-content">
+            
+            <div class="content">
+                <div class="card">At a Glance</div>
+                <div class="card">Quick Draft</div>
+                <div class="card">Activity</div>
+                <div class="card">WordPress Events and News</div>
+            </div>
+
+        </div>
+    </div>
+</body>
+</html>
