11b35d9eb86a199357c83edb7c9ed7e13b3ff3dc
Dust-Man <carlosgaelhp@gmail.com>
2024-12-10 19:03:10 -0600

    envios proxims

--- dashboard/insertar_envio.php
+++ dashboard/insertar_envio.php
@@ -11,9 +11,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $empleado_id = $_POST['empleado_id'];
         $ruta = $_POST['ruta'];
         $activo = isset($_POST['tienda']) ? true : false;
+        $direccion = $_POST['direccion'];
 
-        $sqlEnvio = "INSERT INTO envios (nota_id, vehiculo_id, hora, fecha_envio,empleado_id, ruta)
-                        VALUES (:nota_id, :vehiculo_id, :hora, :fecha_envio,:empleado_id, :ruta)";
+        $sqlEnvio = "INSERT INTO envios (nota_id, vehiculo_id, hora, fecha_envio,empleado_id, ruta, direccion)
+                        VALUES (:nota_id, :vehiculo_id, :hora, :fecha_envio,:empleado_id, :ruta, :direccion)";
         $stmtEnvio = $conexion->prepare($sqlEnvio);
         $stmtEnvio->execute([
             ':nota_id' => $nota_id,
@@ -22,6 +23,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             ':fecha_envio' => $fecha,
             ':empleado_id' => $empleado_id,
             ':ruta' => $ruta,
+            ':direccion' => $direccion,
         ]);
 
         $envio_id = $conexion->lastInsertId();
@@ -29,18 +31,18 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $productos = $_POST['producto_id'];
         $cantidades = $_POST['cantidad'];
         $ids_prod_env = $_POST['id_prod_env'];
+       
 
         foreach ($productos as $index => $producto_id) {
             $cantidad = $cantidades[$index];
             $id_prod_env = $ids_prod_env[$index];
             if ($cantidad > 0) {
-                $sqlEnvioEncabezado = "INSERT INTO envios_encabezado (nota_id, envio_id,direccion,producto_id, cantidad)
-                                        VALUES (:nota_id, :envio_id,:direccion,:producto_id, :cantidad)";
+                $sqlEnvioEncabezado = "INSERT INTO envios_encabezado (nota_id, envio_id,producto_id, cantidad)
+                                        VALUES (:nota_id, :envio_id,:producto_id, :cantidad)";
                 $stmtEnvioEncabezado = $conexion->prepare($sqlEnvioEncabezado);
                 $stmtEnvioEncabezado->execute([
                     ':nota_id' => $nota_id,
                     ':envio_id' => $envio_id,
-                    ':direccion' => 'ejemplo',
                     ':producto_id' => $producto_id,
                     ':cantidad' => $cantidad,
                 ]);
